50278d6b6ed42fb7089ffdf905612a52fb71c476
tanhLinhphong <pvtuananh1505@gmail.com>
2023-10-16 10:09:00 +0700

    frist commit

---
+++ JavaScript/c02/js/numeric-variable.js
@@ -0,0 +1,8 @@
+var price;
+var quantily;
+var total;
+price=5;
+quantily=14;
+total=price*quantily;
+var el = document.getElementById('cost');
+el.textContent='$'+ total
---
+++ JavaScript/c02/js/update-array.js
@@ -0,0 +1,4 @@
+var color = ['white', 'black', 'custom'];
+color[2] = 'beige';
+var el = document.getElementById('colors');
+el.textContent = color[2];
