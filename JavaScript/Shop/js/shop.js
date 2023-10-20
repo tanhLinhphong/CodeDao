bbce5e24e0a58a473a4fec79ef8c5a9503c59db6
tanhLinhphong <pvtuananh1505@gmail.com>
2023-10-20 08:07:01 +0700

    frist commit

---
+++ JavaScript/Shop/js/shop.js
@@ -0,0 +1,46 @@
+function calculateTotal(){
+    var quantity1=document.getElementById("quantity1").value;
+    var price1=document.getElementById("price1").innerHTML;
+
+    var quantity2=document.getElementById("quantity2").value;
+    var price2=document.getElementById("price2").innerHTML;
+    
+    var quantity3=document.getElementById("quantity3").value;
+    var price3=document.getElementById("price3").innerHTML;
+     
+    var total =quantity1*price1+quantity2*price2+quantity3*price3;
+    if(total>4000){
+        total=total*0.75;
+        document.getElementById("total").style.color="Green"
+    }
+    else if(total<4000 , total>2000){
+        total=total*0.9;
+        document.getElementById("total").style.color="Blue"
+    }
+    else{
+        document.getElementById("total").style.color="Red"
+    }
+    document.getElementById("total").innerHTML="<h3>Tổng Tiền: "+ total + "USD</h3>";
+}
+
+function updateGreeting(){
+    const greetingElement=document.getElementById('greeting');
+    const timeElement=document.getElementById('time');
+    const currentTime=new Date();
+    let className;
+    greetingElement.className=className;
+    const timeString=` ${currentTime.getHours()}:${String(currentTime.getMinutes()).padStart(2,'0')}:${String(currentTime.getSeconds()).padStart(2,'0')}`;
+    timeElement.textContent=timeString;
+}
+setInterval(updateGreeting,1000);
+function changeColor(){
+    const timeElement =document.getElementById('time');
+    const currentColor=timeElement.style.color;
+    if (currentColor=='blue'){
+        timeElement.style.color='red';
+     } else{
+      timeElement.style.color='blue';  
+      }
+ 
+   }
+   setInterval(changeColor,60000)
--- JavaScript/timechange/js/change.js
+++ JavaScript/timechange/js/change.js
@@ -1,57 +1,58 @@
-function updateGreeting() {
-    // var test = 1;
-    // var test;
-    // test = 2;
-    // var: phạm vi toàn cục (golbal),
-    // có thể khai báo lại, không cần giá trị khi khởi tạo
-    // có thể tay đổi cập nhật giá trị
-    //const khai báo hằng số, giá trị không được cập nhật và PHẢI khởi tạo giá trị ban đầu
-    const greetingElement = document.getElementById('greeting');
-    // const greetingElement;
-    // const greetingElement1 = '';
-    const timeElenment = document.getElementById('time');
-    const timeElenment2 = document.getElementById('time2');
-    const secondsElement = document.querySelector('.seconds span');
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-    // let khai báo biến, không được khai báo lại, có thể cập nhật giá trị
-    let greeting = '';
-    let className = '';
-    // let greeing;
-    // let className;
-    if (currentHour < 12) {
-        greeting = 'Chao buổi sáng';
-        className = 'morning';
-    } else if (currentHour < 18) {
-        greeting = 'Chao buổi chiều';
-        className = 'afternoon';
-    } else {
-        greeting = 'Chao buổi tối';
-        className = 'evening';
-    }
-    greetingElement.textContent = greeting;
-    greetingElement.className = className;
-    const timeString = `Thoi gian đầy đủ: ${currentTime}`;
-    const timeString2 = `Thoi gian rút gọn: ${currentTime.getHours()}:${String(currentTime.getMinutes()).padStart(2, '0')}:${String(currentTime.getSeconds()).padStart(2, '0')}`;
-    timeElenment.textContent = timeString;
-    timeElenment2.textContent = timeString2;
-    secondsElement.textContent = currentTime.getSeconds();
-}
-// Gọi hàm cập nhật mỗi giây
-setInterval(updateGreeting, 1000);
-// cập nhật lần đầu khi tải trang
-updateGreeting();
-function changeColor() {
-    const textElement = document.getElementById('text');
-    // lấy ra màu sắc hiện tại của phần tử
-    const currentColor = textElement.style.color;
-    if (currentColor === 'blue') {
-        textElement.style.color = 'red';
-        textElement.textContent = 'Văn bản màu đỏ rồi sẽ thay màu';
-    } else {
-        textElement.style.color = 'blue';
-        textElement.textContent = 'Văn bản màu xanh rồi sẽ thay màu';
-    }
-}
-// Gọi hàm thay đổi màu mỗi giây
-setInterval(changeColor, 1000);
+ function updateGreeting(){
+    // var test=1;
+    // var test=2;
+    // test=2;
+    // var: phạm vi toàn cục(golbal)
+    // có thể khai báo lại , không cần gán giá trị khi khởi tạo
+    //  có thể thay đổi cập nhập giá trị
+    //  const khai báo hằng số, giá trị không được cập nhẩk và phải khởi tạo giá trị ban đầu
+     const greetingElement=document.getElementById('greeting');
+    //  const timeElement=document.getElementById('time');
+    const timeElement2=document.getElementById('time2');
+    // const secondsElement=document.querySelector('.seconds span');
+    const currentTime= new Date();
+    // const currentHour=currentTime.getHours();
+
+    // let khai báo biến , khồn được khai báo lại, không cần giá trị ban đầu
+    
+    // let greeting;
+     let className;
+    //  if (currentHour<12){
+    //     greeting='Chào buổi sáng';
+    //     className='morning';
+    //  }else if(currentHour<18){
+    //     greeting='chào buổi chiều';
+    //     className='afternoon';
+    //  }else{
+    //     greeting='chào buổi tối';
+    //     className='evening';
+    //  }
+    //  greetingElement.textContent=greeting;
+     greetingElement.className=className;
+
+    //  const timeString=`Thời Gian Đầy Đủ:: ${currentTime}`;
+     const timeString2=`Thời Gian Rút Gọn:: ${currentTime.getHours()}:${String(currentTime.getMinutes()).padStart(2,'0')}:${String(currentTime.getSeconds()).padStart(2,'0')}`;
+    //  timeElement.textContent=timeString;
+    timeElement2.textContent=timeString2;
+
+    //  secondsElement.textContent=currentTime.getSeconds();
+
+ }
+// Gọi Hàm Cập Nhật mỗi giây
+ setInterval(updateGreeting,1000);
+// // Cập Nhật lần đầu khi tải trang
+// // updateGreeting();
+  function changeColor(){
+    const textElement =document.getElementById('text');
+   // Lấy ra màu sắc hiện tại của phần tử
+   const currentColor=textElement.style.color;
+   if (currentColor=='blue'){
+       textElement.style.color='red';
+      textElement.textContent='Văn bản màu đỏ rồi sẽ thay màu';
+    } else{
+       textElement.style.color='blue';
+         textElement.textContent='Văn bản màu xanh rồi sẽ thay màu';
+     }
+
+  }
+  setInterval(changeColor,60000)
