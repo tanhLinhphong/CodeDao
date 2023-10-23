b954ffe521aac83435bbf31c3018db5fc532575b
tanhLinhphong <pvtuananh1505@gmail.com>
2023-10-23 10:00:46 +0700

    frist commit

---
+++ JavaScript/DOMMMM/js/index.js
@@ -0,0 +1,16 @@
+const myList=document.getElementById('myList');
+
+const newList1=document.createElement("li");
+newList1.textContent="Con Mèo";
+myList.insertBefore(newList1,myList.firstChild);
+
+const newList2=document.createElement("li");
+newList2.textContent="Con lợn";
+myList.appendChild(newList2);
+
+const liList=myList.getElementsByTagName('li');
+for (let i=0;i<liList.length;i++){
+    liList[i].classList.add('dark');
+}
+const header=document.getElementById("header");
+header.textContent=`Tổng số lượng phần tử: ${liList.length}`;
